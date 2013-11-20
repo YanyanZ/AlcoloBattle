cb930f2460d14546c998f3d835e06294aa969dea
Caroline Beyne <caroline.beyne@gmail.com>
2013-11-20 17:25:36 +0100

    setEntityBoolean with PARAMETERS OYEAH

--- implementation/src/ImplementationLibrary.cpp
+++ implementation/src/ImplementationLibrary.cpp
@@ -1,43 +1,40 @@
 #include "ImplementationLibrary.h"
 
-namespace templates
+namespace alcolo
 {
-    namespace impl_lib
-    {
-        ImplementationLibrary::ImplementationLibrary()
-            : AbstractImplementationLibrary("templates")
-            , customAction_()
-        {
-            // NOTHING
-        }
+	ImplementationLibrary::ImplementationLibrary()
+		: AbstractImplementationLibrary("alcolo")
+		, setEntityBoolean_()
+	{
+		// NOTHING
+	}
 
-        ImplementationLibrary::~ImplementationLibrary()
-        {
-            if (isLoaded())
-                forceUnload();
-        }
+	ImplementationLibrary::~ImplementationLibrary()
+	{
+		if (isLoaded())
+			forceUnload();
+	}
 
-        bool ImplementationLibrary::doLoad(mlv::utils::AbstractLogger* logger)
-        {
-            bool success = true;
-			
-			//Actions
-			success = success && registerType<setSecuEngageParams>(logger);
-            success = success && registerAction(customAction_, logger);		
-			
-            return success;
-        }
+	bool ImplementationLibrary::doLoad(mlv::utils::AbstractLogger* logger)
+	{
+		bool success = true;
+		
+		//Actions
+		success = success && registerType<SetEntityBooleanParams>(logger);
+		success = success && registerAction(setEntityBoolean_, logger);		
+		
+		return success;
+	}
 
-        bool ImplementationLibrary::doUnload(mlv::utils::AbstractLogger* logger)
-        {
-            bool success = true;
+	bool ImplementationLibrary::doUnload(mlv::utils::AbstractLogger* logger)
+	{
+		bool success = true;
+		
+		//Actions
+		success = success && unregisterAction(setEntityBoolean_, logger);
+		if (success)
+			unregisterType<SetEntityBooleanParams>();
 			
-			//Actions
-			success = success && unregisterAction(customAction_, logger);
-            if (success)
-                unregisterType<setSecuEngageParams>();
-				
-            return success;
-        }
-    }
+		return success;
+	}
 }
